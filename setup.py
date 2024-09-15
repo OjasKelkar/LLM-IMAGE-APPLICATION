970aa3b74f6cbf4d1838ea18713cc13dc5e85e95
OjasKelkar <145983873+OjasKelkar@users.noreply.github.com>
2024-09-15 17:05:07 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'genai',
+    version= '0.0.1',
+    author= 'Ojas R',
+    author_email= 'kelkarork0308@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
